1c2f7cc873fac1b843f3399101c8aedb813338ab
Patrick Debois <70908+jedi4ever@users.noreply.github.com>
2025-01-27 18:25:30 +0100

    a few simple store tests

---
+++ frontend/src/stores/__tests__/toolLandscape.spec.ts
@@ -0,0 +1,30 @@
+import { describe, it, beforeEach, expect } from 'vitest';
+import { useToolLandscapeStore } from '@/stores/toolLandscape';
+import { createPinia , setActivePinia} from 'pinia';
+
+describe('LandscapeStore', () => {
+    let toolLandscapeStore;
+
+    beforeEach(() => {
+        setActivePinia(createPinia())
+        toolLandscapeStore = useToolLandscapeStore();
+    });
+
+    it('should initialize with an empty landscape', () => {
+        expect(toolLandscapeStore.getAllDomains).toEqual([]);
+    });
+
+    it('should add a domain', () => {
+        const domain = { id: 1, name: 'Mountain' };
+        toolLandscapeStore.addDomain(domain);
+        expect(toolLandscapeStore.getAllDomains).toEqual([domain]);
+    });
+
+    // test for delete of a domain
+    it('should delete a domain', () => {
+        const domain = { id: 1, name: 'Mountain' };
+        toolLandscapeStore.addDomain(domain);
+        toolLandscapeStore.deleteDomain(domain.id);
+        expect(toolLandscapeStore.getAllDomains).toEqual([]);
+    });
+});
--- frontend/src/stores/toolLandscape.ts
+++ frontend/src/stores/toolLandscape.ts
@@ -17,15 +17,31 @@ export const useToolLandscapeStore = defineStore('toolLandscape', {
         addDomain(domain: ToolDomain) {
             this.domains.push(domain)
         },
-        addCategory(category: ToolCategory) {
-            this.categories.push(category)
+
+        deleteDomain(domain: ToolDomain) {
+            this.domains = this.domains.filter(d => d.uid !== domain.uid)
         },
+
         getDomainById(uid: string) {
             return this.domains.find(domain => domain.uid === uid)
         },
+
+        addCategory(category: ToolCategory) {
+            this.categories.push(category)
+        },
+
+        deleteCategory(category: ToolCategory) {
+            this.categories = this.categories.filter(c => c.uid !== category.uid)
+        },
+
         getCategoriesByDomainId(domainId: string) {
             return this.categories.filter(category => category.domainId === domainId)
         },
+
+        getCategoryById(uid: string) {
+            return this.categories.find(category => category.uid === uid)
+        },
+
         initializeFromYaml() {
             const data = toolData as { domains: ToolDomain[], categories: ToolCategory[] };
             data.domains.forEach(domain => this.addDomain(domain));
