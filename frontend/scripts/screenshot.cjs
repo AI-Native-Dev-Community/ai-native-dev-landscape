d86b6834d95c509ebc6145517404bf8cdffab374
Patrick Debois <70908+jedi4ever@users.noreply.github.com>
2025-02-11 15:53:44 +0100

    feat: screenshot with correct width

--- frontend/scripts/screenshot.cjs
+++ frontend/scripts/screenshot.cjs
@@ -10,6 +10,8 @@ const path = require('path');
     // Navigate to the desired URL
     for (const url of ['/landscape', '/catalog', '/awesome']) {
         await page.goto("http://localhost:4173" + url)
+        // set size to 1920x1080
+        await page.setViewport({ width: 1920, height: 1080 });
         await page.waitForNetworkIdle();
 
         // Take a screenshot and save it in the 'dist' directory
